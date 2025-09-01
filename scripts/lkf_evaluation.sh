ccc02b742c2a0c9b55cd3b3b01aeb7ee0e6c12d1
nmndeep <nmndeep@gmail.com>
2025-09-01 17:42:52 +0200

    Update lkf_evaluation.sh

--- scripts/lkf_evaluation.sh
+++ scripts/lkf_evaluation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# conda activate LKFJensUn
+# conda activate lkfjensun
 
 export MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()")
 echo "Master Port: $MASTER_PORT"
